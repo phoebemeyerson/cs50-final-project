1a05e37027f8f152b4460777e5e8768da99047d9
Phoebe Meyerson <phoebemeyerson@dhcp-10-250-227-5.harvard.edu>
2021-12-06 15:26:15 -0500

    commit

--- sql.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('noms.db')
-c = conn.cursor()
-
-c.execute('''
-          CREATE TABLE IF NOT EXISTS users
-          ([id] INTEGER PRIMARY KEY, [username] TEXT NOT NULL, [hash] TEXT NOT NULL)
-          ''')
